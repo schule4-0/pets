45ffe0d46838e97cd191580114f12b647dd0b962
jb266 <jb266@hdm-stuttgart.de>
2024-05-02 18:03:39 +0200

    quick and dirty drag and drop implementation

---
+++ src/components/DragItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="square" :class="{ active: isActive }">
+    <span>{{ text }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    isActive: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.square {
+  width: 100px;
+  height: 100px;
+  background-color: #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.active {
+  background-color: #ff0000;
+}
+</style>
--- src/views/dog/MinigameTwo.vue
+++ src/views/dog/MinigameTwo.vue
@@ -1,16 +1,112 @@
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const items = ref([
+      { id: 0, title: 'Leine', correct: true, list: 1 },
+      { id: 1, title: 'Mülltüte', correct: true, list: 1 },
+      { id: 2, title: 'Leckerli', correct: true, list: 1 },
+      { id: 3, title: 'Schrank', correct: false, list: 1 },
+      { id: 4, title: 'Schulbücher', correct: false, list: 1 },
+      { id: 5, title: 'Stift', correct: false, list: 1 }
+    ])
+
+    // get the items for a specific list
+    const getList = (list) => {
+      return items.value.filter((item) => item.list == list)
+    }
+
+    // start event handler for drag
+    const startDrag = (event, item) => {
+      // pass the item id to the dataTransfer object
+      event.dataTransfer.setData('itemID', item.id)
+    }
+
+    // drop event handler
+    const onDrop = (event, list) => {
+      const itemID = event.dataTransfer.getData('itemID')
+      const droppedItem = items.value.find((item) => item.id == itemID)
+      console.log('droppedItem: ', droppedItem)
+      console.log('list: ', list)
+
+      if (droppedItem.correct) {
+        // logic for correct item
+        droppedItem.list = list
+        console.log('richtiges Item')
+      } else {
+        // logic for wrong item
+        console.log('falsches Item')
+      }
+    }
+
+    return {
+      getList,
+      onDrop,
+      startDrag
+    }
+  }
+}
+</script>
+
 <template>
-  <div>
-    <example-component>
-      <template #game-title>
-        <h2>Zwei</h2>
-      </template>
-      <template #game-description>
-        <div>Wir befinden uns im zweiten Minigame.</div>
-      </template>
-    </example-component>
+  <!-- item list -->
+  <div class="item-zone" @drop="onDrop($event, 1)" @dragenter.prevent @dragover.prevent>
+    <div><h3>items</h3></div>
+
+    <div
+      v-for="item in getList(1)"
+      :key="item.id"
+      class="drag-el"
+      draggable="true"
+      @dragstart="startDrag($event, item)"
+    >
+      {{ item.title }}
+    </div>
+  </div>
+
+  <!-- destination -->
+  <div class="drop-zone" @drop="onDrop($event, 2)" @dragenter.prevent @dragover.prevent>
+    <div><h3>Rucksack</h3></div>
+    <div
+      v-for="item in getList(2)"
+      :key="item.id"
+      class="drag-el"
+      draggable="false"
+      @dragstart="startDrag($event, item)"
+    >
+      {{ item.title }}
+    </div>
   </div>
 </template>
 
-<script setup lang="ts">
-import ExampleComponent from '@/components/ExampleComponent.vue'
-</script>
+<style>
+#app {
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-smoothing: grayscale;
+  text-align: center;
+  color: beige;
+}
+
+.drop-zone {
+  width: 250px;
+  height: 250px;
+  margin: 50px auto;
+  border: 0.3cm dashed black;
+  padding: 10px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  flex-direction: row;
+}
+
+.drag-el {
+  background-color: aquamarine;
+  color: rgb(144, 92, 92);
+  width: 100px;
+  height: 50px;
+  margin: 5px;
+}
+</style>
