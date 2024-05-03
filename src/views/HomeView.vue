dac56aa076770adcd22fa4a803238b9f86f7d582
Johanna Rauscher <jr132@hdm-stuttgart.de>
2024-05-03 10:10:56 +0200

    Homeview draft

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="home-container">
-    <h1>Schule 4.0 - Haustiere</h1>
-    <p>Wähle ein Haustier um das Spiel zu starten:</p>
-    <select v-model="selectedPet" @change="goToPetStages">
-      <option disabled value="">Wähle ein Haustier</option>
-      <option value="dog">Hund</option>
-    </select>
+    
+    <div class="header-box">
+      <h1>Mein erstes Haustier</h1>
+      <img src="../assets/icon_audio.svg" alt="Audio">
+    </div>
+
+    <p>Bereit, in die Welt der Haustiere einzutauchen?
+    <br>Tippe auf das Tier, das dich am meisten interessiert!</p>
+  </div>
+
+  <div class="pet-selection">
+    <div class="pet-tile" @click="selectPet('dog')">
+      <img src="../assets/dog.png" alt="Hund">
+      <div class="pet-tile-name">
+        <p>Der Hund</p>
+        <img src="../assets/icon_audio.svg" alt="Audio">
+      </div>
+    </div>
+    <div class="pet-tile" @click="selectPet('dog')">
+      <img src="../assets/bunny.png" alt="">
+      <div class="pet-tile-name">
+        <p>Das Kaninchen</p>
+        <img src="../assets/icon_audio.svg" alt="Audio">
+      </div>
+    </div>
+    <div class="pet-tile" @click="selectPet('dog')">
+      <img src="../assets/dog.png" alt="">
+      <div class="pet-tile-name">
+        <p>Die Katze</p>
+        <img src="../assets/icon_audio.svg" alt="Audio">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,6 +41,11 @@ import { useRouter } from 'vue-router'
 
 const selectedPet = ref('')
 const router = useRouter()
+
+const selectPet = (pet: string) => {
+  selectedPet.value = pet
+  goToPetStages()
+}
 
 const goToPetStages = () => {
   if (selectedPet.value) {
@@ -28,14 +59,64 @@ const goToPetStages = () => {
   margin: auto;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  padding: 20px;
+  align-items: left;
+  font-size: 50px;
+  line-height: 100%;
+
+  h1 {
+    font-size: 80px;
+    padding: 40px 0;
+  }
+
+  p {
+    padding: 20px 0;
+  }
+
+  .header-box {
+    display: flex;
+
+    img {
+      width: 75px;
+      margin: 15px 0 15px 40px;
+    }
+  }
 }
 
-select {
-  margin-top: 10px;
-  padding: 8px;
-  font-size: 16px;
-  width: 200px;
+.pet-selection {
+  margin-top: 40px;
+  display: flex;
+  justify-content: space-between;
+
+  .pet-tile {
+    background-color: rgb(231, 231, 231);
+    box-shadow: 3px 3px 15px rgb(186, 186, 186);
+    width: 31%;
+    position: relative;
+
+    img {
+      width: 100%;
+      height: 400px;
+      object-fit: cover;
+      background-position: center;
+    }
+  }
+
+  .pet-tile-name {
+    font-size: 40px;
+    padding: 15px;
+    display: flex;
+    justify-content: space-between;
+
+    p {
+      padding-top: 10px;
+    }
+
+    img {
+      max-width: 10%;
+      height: auto;
+      margin: 15px;
+    }
+  }
 }
+
 </style>
