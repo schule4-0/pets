f8f75a32b7b106bff23289af9d006f34a224ba8f
Steffen <ss546@hdm-stuttgart.de>
2024-06-05 11:51:12 +0200

    #27 Rough implementation of the nutrition game

---
+++ src/views/dog/MinigameFood.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="game">
+    <DogBowl />
+    <FoodItem v-for="food in foods" :key="food.id" :food="food" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useGameStore } from '@/stores/nutritionGameStore'
+import DogBowl from '@/components/DogBowl.vue'
+import FoodItem from '@/components/FoodItem.vue'
+import { storeToRefs } from 'pinia'
+
+const { foods } = storeToRefs(useGameStore())
+const { startGame } = useGameStore()
+
+onMounted(() => {
+  startGame()
+})
+</script>
+
+<style scoped>
+.game {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
