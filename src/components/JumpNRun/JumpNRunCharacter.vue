5f74dac6600f6807d73fcbe6de48b9e1c16a8e49
jb266 <jb266@hdm-stuttgart.de>
2024-06-12 18:48:15 +0200

    use gsap for jump animation

--- src/components/JumpNRun/JumpNRunCharacter.vue
+++ src/components/JumpNRun/JumpNRunCharacter.vue
@@ -1,14 +1,40 @@
 <template>
-  <div :class="{ character: true, jump: props.isJumping }">
+  <div :class="{ character: true, jump: isJumping }" ref="characterRef">
     <img :src="image" alt="dog" class="character-img" />
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, watch } from 'vue'
+import gsap from 'gsap'
+
 const props = defineProps<{
   image: string
   isJumping: boolean
 }>()
+
+const characterRef = ref<HTMLElement | null>(null)
+
+watch(
+  () => props.isJumping,
+  (newValue) => {
+    if (newValue) {
+      jumpAnimation()
+    }
+  }
+)
+
+const jumpAnimation = () => {
+  if (characterRef.value) {
+    gsap.to(characterRef.value, {
+      duration: 1.5,
+      y: -400,
+      ease: 'power2.out',
+      yoyo: true,
+      repeat: 1
+    })
+  }
+}
 </script>
 
 <style scoped>
@@ -16,22 +42,6 @@ const props = defineProps<{
   position: absolute;
   bottom: 20vh;
   left: 50vh;
-}
-
-.character.jump {
-  animation: jump-animation 3s forwards;
-}
-
-@keyframes jump-animation {
-  0% {
-    bottom: 20vh;
-  }
-  50% {
-    bottom: 60vh;
-  }
-  100% {
-    bottom: 20vh;
-  }
 }
 
 .character-img {
